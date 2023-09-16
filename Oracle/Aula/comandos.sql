3e707a83580ba58592cc7939177764074906b8b9
Lucas Oliveira <lucasaugustooliveira75@gmail.com>
2023-09-15 22:36:06 -0300

    Atualizando comandos

--- Oracle/Aula/comandos.sql
+++ Oracle/Aula/comandos.sql
@@ -1,37 +1,23 @@
 --TABELA CLIENTE
-
 select * from TABELA_DE_CLIENTES;
-
 select * from TABELA_DE_CLIENTES where data_de_nascimento >= to_date('14/11/1995','DD/MM/YYYY');
-
 select * from TABELA_DE_CLIENTES where IDADE BETWEEN 18 AND 22;
-
 select * from TABELA_DE_CLIENTES where NOME LIKE '%Silva%';
-
 select cidade, round(avg(idade),2) from TABELA_DE_CLIENTES GROUP BY cidade;
 
 -- HAVING FAZ FILTROS CONDICIONAIS DO RESULTADO QUE É FEITO DO SELEC, PORTANTO ELE FICA NO FINAL
 select estado, sum(limite_de_credito) from TABELA_DE_CLIENTES group by estado having sum(limite_de_credito) >= 900000;
-
 select distinct tc.bairro from tabela_de_clientes tc union select distinct tv.bairro from tabela_de_vendedores tv;
-
 select distinct tc.bairro from tabela_de_clientes tc union all select distinct tv.bairro from tabela_de_vendedores tv;
-
 select * from tabela_de_clientes where bairro in (select distinct bairro from tabela_de_vendedores);
 
 
 --TABELA PRODUTO
-
 select * from TABELA_DE_PRODUTOS;
-
 select * from TABELA_DE_PRODUTOS where sabor in ('Manga', 'Laranja', 'Melancia') and tamanho = '1 Litro';
-
 select * from TABELA_DE_PRODUTOS where sabor in ('Manga', 'Laranja', 'Melancia') and (preco_de_lista between 8 and 15);
-
 select distinct embalagem from TABELA_DE_PRODUTOS;
-
 select rownum, p.* from TABELA_DE_PRODUTOS p where rownum <= 4;
-
 select nome_do_produto, preco_de_lista, 
     (case when preco_de_lista >= 12 then 'PRODUTO CARO' 
           when preco_de_lista >= 7 and preco_de_lista < 12 then 'PRODUTO EM CONTA' 
@@ -49,7 +35,6 @@ group by nf.matricula, tv.nome;
 
 
 --VIEW
-
 create view vw_soma_embalagens as 
 select embalagem, sum(preco_de_lista) as soma_preco
 from tabela_de_produtos group by embalagem;
@@ -57,7 +42,6 @@ from tabela_de_produtos group by embalagem;
 select * from vw_soma_embalagens;
 
 --FUNÇOÕES PARA TEXTO
-
 select nome, lower(nome) from tabela_de_clientes; 
 select nome, upper(nome) from tabela_de_clientes;
 select nome, initcap(nome) from tabela_de_clientes;
@@ -94,7 +78,6 @@ select nome_do_produto, replace(nome_do_produto, 'Litro', 'L') from tabela_de_pr
 -- HH24 - HORA DE 0 A 24
 -- MI - MINUTO
 -- SS - SEGUNDO
-
 select sysdate from dual;
 select sysdate+127 from dual;
 select to_char(sysdate, 'DD/MM/YYYY') from dual;
@@ -108,7 +91,6 @@ select sysdate, trunc(sysdate, 'month') from dual;
 select extract(month from to_date('05/09/2023','dd/mm/yyyy')) from dual;
 
 --FUNÇOÕES MATEMÁTICAS
-
 select ceil(3.6) from dual;
 select floor(3.6) from dual;
 select power(10,4) from dual;
@@ -157,3 +139,11 @@ ITENS_NOTAS ITN
 ON N.NUMERO = ITN.NUMERO
 GROUP BY N.DATA_VENDA;
 END;
+
+--CRIANDO UM USUARIO
+ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
+CREATE USER lucas IDENTIFIED BY lucas DEFAULT TABLESPACE USERS;
+-- DANDO ACESSO TOTAL AO USUARIO CRIADO
+GRANT CONNECT, RESOURCE TO LUCAS;
+-- ADICIONANDO UMA COTA ILIMITADA DE TAMANHO DA TABLESPACE (AREA DE TRABALHO)
+ALTER USER lucas QUOTA UNLIMITED ON USERS;
